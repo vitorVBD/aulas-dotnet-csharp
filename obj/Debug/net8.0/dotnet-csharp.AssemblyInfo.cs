9cedd21c1848b799c89fa219f5d6c933c3037f2e
Vitor Bittencourt <vitor_bittencourt1998@hotmail.com>
2024-09-25 18:50:03 -0300

    adicionando mais anotações

--- obj/Debug/net8.0/dotnet-csharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef9ec603f510d49db55e1e199806097b257112d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
